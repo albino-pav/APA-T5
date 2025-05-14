178490b959a6b834e32113aa97a122b8b92aa373
Alex Segura <alex.segura.medina@estudiantat.upc.edu>
2025-05-14 16:30:12 +0200

    funciones

--- estereo.py
+++ estereo.py
@@ -52,3 +52,90 @@ def estereo2mono(ficEste, ficMono, canal = 2):
         fm.write(struct.pack('<' + 'h'*len(canal_mono), *canal_mono))
 
 
+def mono2estereo(ficIzq, ficDer, ficEste):
+    """Genera un archivo WAVE estéreo combinando dos archivos WAVE mono.
+
+    Cada archivo debe tener el mismo número de muestras, frecuencia de muestreo
+    y formato (PCM 16-bit). El canal izquierdo se toma de ficIzq y el derecho de ficDer.
+    """
+    with open(ficIzq, 'rb') as fz:
+        header_izq = fz.read(44)
+        if header_izq[0:4] != b'RIFF' or header_izq[8:12] != b'WAVE':
+            raise ValueError("ficIzq no es un fichero WAVE válido")
+        fmt_tag, nchannels, sample_rate, _, _, bps = struct.unpack('<HHIIHH', header_izq[20:36])
+        if nchannels != 1 or fmt_tag != 1 or bps != 16:
+            raise ValueError("ficIzq no es un fichero mono PCM 16-bit válido")
+        data_izq = fz.read()
+
+    with open(ficDer, 'rb') as fd:
+        header_der = fd.read(44)
+        if header_der[0:4] != b'RIFF' or header_der[8:12] != b'WAVE':
+            raise ValueError("ficDer no es un fichero WAVE válido")
+        fmt_tag_d, nchannels_d, sample_rate_d, _, _, bps_d = struct.unpack('<HHIIHH', header_der[20:36])
+        if nchannels_d != 1 or fmt_tag_d != 1 or bps_d != 16:
+            raise ValueError("ficDer no es un fichero mono PCM 16-bit válido")
+        if sample_rate_d != sample_rate or bps_d != bps:
+            raise ValueError("Los ficheros mono no tienen la misma configuración")
+        data_der = fd.read()
+
+    muestras_izq = struct.unpack('<' + 'h' * (len(data_izq) // 2), data_izq)
+    muestras_der = struct.unpack('<' + 'h' * (len(data_der) // 2), data_der)
+    if len(muestras_izq) != len(muestras_der):
+        raise ValueError("Los ficheros mono no tienen el mismo número de muestras")
+
+    intercaladas = [val for pair in zip(muestras_izq, muestras_der) for val in pair]
+    data_size = len(intercaladas) * 2  # 2 bytes por muestra
+    byte_rate = sample_rate * 4        # 2 canales x 2 bytes
+    block_align = 4                    # 2 canales x 2 bytes
+
+    with open(ficEste, 'wb') as fe:
+        fe.write(b'RIFF')
+        fe.write(struct.pack('<I', 36 + data_size))
+        fe.write(b'WAVE')
+        fe.write(b'fmt ')
+        fe.write(struct.pack('<IHHIIHH', 16, 1, 2, sample_rate,
+                             byte_rate, block_align, 16))
+        fe.write(b'data')
+        fe.write(struct.pack('<I', data_size))
+        fe.write(struct.pack('<' + 'h' * len(intercaladas), *intercaladas))
+
+
+def decEstereo(ficCodif, ficEste):
+    """Decodifica un archivo mono codificado diferencialmente a estéreo PCM 16-bit.
+
+    ficCodif debe contener las diferencias dL, dR. El resultado se guarda en ficEste.
+    """
+    with open(ficCodif, 'rb') as f:
+        cabecera = f.read(44)
+        if cabecera[0:4] != b'RIFF' or cabecera[8:12] != b'WAVE':
+            raise ValueError("ficCodif no es un fichero WAVE válido")
+
+        fmt_tag, nchannels, sample_rate, _, _, bps = struct.unpack('<HHIIHH', cabecera[20:36])
+        if fmt_tag != 1 or nchannels != 1 or bps != 16:
+            raise ValueError("ficCodif no es mono PCM 16-bit")
+
+        data = f.read()
+
+    diffs = struct.unpack('<' + 'h' * (len(data) // 2), data)
+    if len(diffs) % 2 != 0:
+        raise ValueError("Número impar de diferencias. Se esperaban pares dL, dR")
+
+    # Reconstrucción de L y R: L = (dL + dR)/2, R = (dR + dL)/2 (equivalente)
+    canal_izq = [(dl - dr) // 2 for dl, dr in zip(diffs[::2], diffs[1::2])]
+    canal_der = [(dr - dl) // 2 for dl, dr in zip(diffs[::2], diffs[1::2])]
+
+    intercalado = [val for par in zip(canal_izq, canal_der) for val in par]
+    data_size = len(intercalado) * 2
+    byte_rate = sample_rate * 4  # 2 canales * 2 bytes
+    block_align = 4
+
+    with open(ficEste, 'wb') as f:
+        f.write(b'RIFF')
+        f.write(struct.pack('<I', 36 + data_size))
+        f.write(b'WAVE')
+        f.write(b'fmt ')
+        f.write(struct.pack('<IHHIIHH', 16, 1, 2, sample_rate,
+                            byte_rate, block_align, 16))
+        f.write(b'data')
+        f.write(struct.pack('<I', data_size))
+        f.write(struct.pack('<' + 'h' * len(intercalado), *intercalado))
